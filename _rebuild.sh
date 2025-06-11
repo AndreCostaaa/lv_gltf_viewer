7e6ea04c73a278285fd9c8d509f49c88d3ba8870
Matt Kimball <greyworld@gmail.com>
2025-06-11 08:31:43 +0100

    torusknot.gltf material changes and _rebuild.sh default app settings changed

--- _rebuild.sh
+++ _rebuild.sh
@@ -2,5 +2,5 @@ clear
 make -C build -j$(nproc) lvgl_workspace
 if [ $? == 0 ]
 then
-    ./build/lvgl_workspace ./gltfs/torusknot.gltf -aa 2 -env media/hdr/directional.jpg -bg 0
+    ./build/lvgl_workspace ./gltfs/torusknot.gltf -aa 2 -env 7 -bg 0 -spin_rate 60 -yaw 0 -pitch 0 -distance 900
 fi
